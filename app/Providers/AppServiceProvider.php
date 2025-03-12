6ab9efabee34b1cdeed5310aca94784435690d47
yamada5131 <son.nl215131@gmail.com>
2025-03-12 13:15:13 +0000

    feat: 🎸 Enable strict model guarding and unguarding in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -23,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         // JsonResource::withoutWrapping();
+        Model::unguard();
+
+        Model::shouldBeStrict();
     }
 }
