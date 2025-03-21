0b0eaf6c063e4a06d35ac229ae7e0ddf06f9e908
yamada5131 <son.nl215131@gmail.com>
2025-03-21 13:31:15 +0000

    refactor: 🔄 Enhance AppServiceProvider with strict types and modular configuration methods

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,13 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -22,9 +27,54 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
-        // JsonResource::withoutWrapping();
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+        // $this->configureUrls();
+        $this->configureVite();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    /**
+     * Configure the application's dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the application's models.
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
 
         Model::shouldBeStrict();
+    }
+
+    /**
+     * Configure the application's URLs.
+     */
+    private function configureUrls(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    /**
+     * Configure the application's Vite instance.
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 }
